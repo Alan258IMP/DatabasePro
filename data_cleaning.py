ae4dc4353ba7ed87d19af6fd8553ecb2ef104579
Wentao Li <wentao.li18@imperial.ac.uk>
2023-08-13 14:42:07 +0800

    Add basic functions for utils and cleaning scripts

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,3 +1,9 @@
+import numpy as np
+import pandas as pd
+
 class DataCleaner:
     def __init__(self, **kwargs):
         pass
+    def clean_card_data(self, df):
+        df = df.dropna()
+        # TODO: clean & reformat all columns
--- data_extraction.py
+++ data_extraction.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 import requests
 import sqlalchemy as sa
+from tabula import read_pdf
 
 from database_utils import DatabaseConnector
 from data_cleaning import DataCleaner
@@ -18,6 +19,12 @@ class DataExtractor:
     def list_db_tables(self):
         inspector = sa.inspect(self.engine)
         return inspector.get_table_names()
+    
+    def retrieve_pdf_data(self, src):
+        df = read_pdf(src, pages='all') # "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
+        data = pd.concat(df, ignore_index=True)
+        df2 = pd.DataFrame(data)
+        return df2
 
 if __name__ == "__main__":
     connector = DatabaseConnector()       
